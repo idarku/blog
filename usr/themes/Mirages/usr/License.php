4c1b566a3979fdfc317a81cb7834ab475fc743d2
“idarku” <i.1999@qq.com>
2021-12-06 12:22:48 +0800

    ii.do

---
+++ usr/themes/Mirages/usr/License.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+/**
+ * License.php
+ * Author     : Hran
+ * Date       : 2020/07/11
+ * Version    :
+ * Description:
+ */
+class License {
+    static $name = "Mirages";
+    static $pk = "UENQbGxVYToxNTk0Mzk4MTUwOmQ4YTg4OTQwZDcxMmVkNGQ5Mzg5MjY4MzgwMTQ3NDA20";
+}
